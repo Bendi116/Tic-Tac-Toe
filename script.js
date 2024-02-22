d6c91d3dc782f79fdfb79cc74c443041371f2a5d
Bendi116 <142507571+Bendi116@users.noreply.github.com>
2024-02-22 15:19:27 +0100

    MAke gameControll and a working version in console.

--- script.js
+++ script.js
@@ -2,15 +2,18 @@
 const gameBoard = (function(){
     const gameBoardArray = [["","",""],["","",""],["","",""]];
     const dispalyBoard = () => {for (let i = 0; i < gameBoardArray.length; i++) { console.log(gameBoardArray[i])}};
+    const tileIsFree = (pos) => {return gameBoardArray[pos[0]][pos[1]] === ""}
     const setTile = (symbol, pos) => {gameBoardArray[pos[0]][pos[1]] = symbol}
-    const notFull = () => {gameBoardArray.forEach(element=>{element.forEach(e=>{if(e === ""){return true;}})});return false;}
+    const notFull = () => {
+        let isTrue = false;
+        gameBoardArray.forEach(element=>{element.forEach(e=>{if(e === ""){isTrue = true;}})});return isTrue;}
     
-    return {dispalyBoard, setTile, gameBoardArray, notFull};
+    return {dispalyBoard, setTile, gameBoardArray, notFull, tileIsFree};
 })();
 
 const gameController= (function(){
-    let currentTurn = "player1"
-
+    let currentPlayer = "";
+    let winCondition = false;
     //inner function[]
     const compare = (arg1,arg2,arg3) => {return (arg1===arg2)&&(arg1==arg3)}
     const checkRow = () =>{            
@@ -32,30 +35,47 @@ const gameController= (function(){
         else if(compare(gameBoard.gameBoardArray[0][2],gameBoard.gameBoardArray[1][1],gameBoard.gameBoardArray[2][0])&& gameBoard.gameBoardArray[0][2] !== ""){return true;}
         else{return false}
     }
+    const logCurrTurn = () => {console.log(`${currentPlayer.name} It's your Turn.`)}
+    const checkWin = () => { if(checkRow() || checkCol() || checkDiagnal()){winCondition = true;}}
 
-    const changeTurn = () => {currentTurn = currentTurn === "player1" ? "player2" : "player1"}
-    const logCurrTurn = () => {console.log(`${changeTurn} It's your Turn.`)}
-    const checkWin = () => { return  checkRow() || checkCol() || checkDiagnal()}
-    return {checkWin, logCurrTurn, changeTurn};
+    const play = () =>{
+        const Player1 = createPlayer()
+        const Player2 = createPlayer()
+        currentPlayer = Player2
+
+        const changePlayer = () => {currentPlayer = currentPlayer === Player1 ? Player2 : Player1}
+        let row;
+        let col;
+        while (gameBoard.notFull() && !winCondition) {
+            changePlayer()
+            logCurrTurn();
+            
+            do{
+                row = prompt(`${currentPlayer.name} Which row you want to place your symbol?`)
+                col = prompt(`${currentPlayer.name} Which col you want to place your symbol?`)
+            }while(!gameBoard.tileIsFree([row,col]))
+            
+
+            gameBoard.setTile(currentPlayer.symbol, [row,col])
+            gameBoard.dispalyBoard()
+
+            checkWin()
+            
+        }
+        if(winCondition){alert(`Game ended. ${currentPlayer.name} win the game`)}
+        else{alert("The game is draw.")}
+        
+    }
+    return {play};
 })();
 
 //factories
-function createPlayer(symbol){
-    let ownSymbol = symbol;
-    const chooseTile = (pos) => {gameBoard.setTile(ownSymbol,pos)};
-    return{chooseTile};
+function createPlayer(){
+    let name = prompt("Player Name:")
+    let symbol = prompt(`${name} symbol?`)
+    return{name,symbol};
 };
 
 
-
-// gameBoard.dispalyBoard()
-// player1 = createPlayer("X");
-// player2 = createPlayer("O");
-// player1.chooseTile([1,2])
-// player2.chooseTile([0,1])
-// gameBoard.dispalyBoard()
-
-gameBoard.dispalyBoard()
-
-let name = prompt("Whats your name ?")
-console.log("Name")
+gameController.play()
+console.log("The game was played.")
